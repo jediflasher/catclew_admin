3d1ec8b6e32e759719d318358c54f5c634f3cd75
Obi <obisfla@gmail.com>
2015-01-09 16:47:35 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CatclewAdmin::Application.config.secret_key_base = 'a4accec6fec976f33a3a16c7731416e1a67400e5ad8a3cd3af42069669785d92c497a3845321dc365d751f7cddaed450cbaf95e19ab92acc84b2fc2325e9ecb5'
